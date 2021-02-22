7da3c80135f91b4eacf8e3e3e467e172476504c8
Glen-Taylor37 <gtaylor37@gmail.com>
2021-02-22 17:32:29 -0500

    Basic Journal Create Route handling POST handling for /journals Applied JSON body parser middleware to express

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
-
 const router = express.Router();
 
 router.get('/', (req, res) => {
 	res.send('Hello World!');
+});
+
+router.post('/journals', (req, res) => {
+	res.send('We got it!');
+	console.log(req.body);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const config = require('./config');
 const usersClient = require('./users/users');
 const router = require('./routes/routes');
 
 const start = async () => {
 	const app = express();
+	app.use(bodyParser.json());
+	app.use(cors());
 	app.use(router);
 
 	app.listen(config.PORT, () => {
